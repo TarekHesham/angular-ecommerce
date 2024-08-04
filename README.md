96a32fc14d7d3a7cfedcdd542ed969ac86b6ef1f
Tarek Hesham <tarekghost6@gmail.com>
2024-08-04 15:43:08 +0300

    First Page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UsersApp
+# ecommerceApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.3.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { UsersListComponent } from './users-list/users-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent, UsersListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'usersApp';
+  title = 'ecommerceApp';
 }
